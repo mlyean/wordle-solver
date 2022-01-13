22265f0be6309e49b67feb68dd95f45c4791c15e
Lim Ming Yean <mingyean@hotmail.com>
2022-01-13 00:10:04 +0000

    Keep track of number of attempts

--- profile.py
+++ profile.py
@@ -22,10 +22,13 @@ if __name__ == "__main__":
             solutions = list(map(str.strip, f.readlines()))
 
         shuffle(solutions)
+
         n = 0
         s = 0
         s2 = 0
         mx = 0
+        stats = [0] * 7
+
         for word in solutions:
             attempts = 0
             with Popen(["./bin/main"], stdin=PIPE, stdout=PIPE, text=True) as proc:
@@ -35,22 +38,31 @@ if __name__ == "__main__":
 
                     result = wordle(word, guess)
 
+                    if result == "ggggg":
+                        proc.kill()
+                        break
+
                     proc.stdin.write(result)
                     proc.stdin.write('\n')
                     proc.stdin.flush()
-
-                    if result == "ggggg":
-                        break
 
             n += 1
             s += attempts
             s2 += attempts ** 2
             mx = max(mx, attempts)
+            if attempts <= 6:
+                stats[attempts] += 1
+            else:
+                stats[0] += 1
 
             if n >= 2:
                 sys.stdout.write('\r')
                 sys.stdout.write(f"N = {n}, μ = {s / n:.5f}, σ = {sqrt((s2 - (s ** 2) / n) / (n - 1)):.5f}, max = {mx}")
                 sys.stdout.flush()
+
+        print()
+        print(f"1: {stats[1]}, 2: {stats[2]}, 3: {stats[3]}, 4: {stats[4]}, 5: {stats[5]}, 6: {stats[6]}, fail: {stats[0]}")
+
     else:
         word = sys.argv[1]
         with Popen(["./bin/main", "-v"], stdin=PIPE, stdout=PIPE, stderr=sys.stderr, text=True) as proc:
